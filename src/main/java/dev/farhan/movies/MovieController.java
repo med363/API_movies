adb6bf2501530f26daedfd3d1d862660a5a13a34
med363 <blibechmohamedamine97@gmail.com>
2026-02-24 17:33:39 +0100

    create first controller exple

---
+++ src/main/java/dev/farhan/movies/MovieController.java
@@ -0,0 +1,16 @@
+package dev.farhan.movies;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+/*fix root*/
+@RequestMapping("/api/v/movies")
+public class MovieController {
+    /*get all*/
+    @GetMapping
+    public String getAllMovies() {
+        return "All Movies";
+    }
+}
