1ec5c0631203b1ddaae8325a57176a0363a92db1
med363 <blibechmohamedamine97@gmail.com>
2026-02-24 17:42:14 +0100

    create first controller with status

--- src/main/java/dev/farhan/movies/MovieController.java
+++ src/main/java/dev/farhan/movies/MovieController.java
@@ -1,5 +1,7 @@
 package dev.farhan.movies;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,7 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class MovieController {
     /*get all*/
     @GetMapping
-    public String getAllMovies() {
-        return "All Movies";
+    public ResponseEntity<String> getAllMovies() {
+        return new ResponseEntity<String>("All movies", HttpStatus.OK);
     }
 }
