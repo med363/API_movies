1ef987199483104ff5d726151da2344efd2b6751
med363 <blibechmohamedamine97@gmail.com>
2026-02-24 17:28:46 +0100

    create second collection and prepare to make rellation between collection

--- src/main/java/dev/farhan/movies/Movies.java
+++ src/main/java/dev/farhan/movies/Movies.java
@@ -6,6 +6,7 @@ import lombok.NoArgsConstructor;
 import org.bson.types.ObjectId;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.DocumentReference;
 
 import java.util.List;
 
@@ -28,6 +29,8 @@ public class Movies {
     private String poster;
     private List<String> genres;
     private List<String> backdrops;
-
+    /*in reference collection*/
+    @DocumentReference
+    private List<String> reviewIds;
 
 }
