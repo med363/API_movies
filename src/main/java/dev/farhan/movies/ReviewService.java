f12e1dc9cf2d44f585b665089dc3c55dbaac105a
med363 <blibechmohamedamine97@gmail.com>
2026-02-25 14:51:26 +0100

    inject mongo template in review service

--- src/main/java/dev/farhan/movies/ReviewService.java
+++ src/main/java/dev/farhan/movies/ReviewService.java
@@ -1,13 +1,16 @@
 package dev.farhan.movies;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ReviewService {
     @Autowired
     private ReviewReposiory reviewReposiory;
-
+/* use fast mongo template WITHout use review repository*/
+    @Autowired
+    private MongoTemplate mongoTemplate;
     public Review createReview(String reviewBody, String imdbId) {
          Review review = new Review(reviewBody);
          reviewReposiory.insert(review);
