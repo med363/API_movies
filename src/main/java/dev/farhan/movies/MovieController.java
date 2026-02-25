a3eaae928323147462057eba76a103bae0500f24
med363 <blibechmohamedamine97@gmail.com>
2026-02-25 17:32:13 +0100

    add two contoller for delete and post movies

--- src/main/java/dev/farhan/movies/MovieController.java
+++ src/main/java/dev/farhan/movies/MovieController.java
@@ -5,10 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
@@ -39,4 +36,63 @@ return new ResponseEntity<Optional<Movies>>(movieService.getSingleMovie(id), Htt
         return new ResponseEntity<Optional<Movies>>(movieService.getSingleMovie(imdbId), HttpStatus.OK);
     }
 
+    /**
+     * POST - Créer un nouveau film
+     * URL: /api/v1/movies
+     * Méthode: POST
+     * Corps: JSON avec les données du film
+     *
+     * Exemple de requête:
+     * {
+     *     "imdbId": "tt1234567",
+     *     "title": "Inception",
+     *     "releaseDate": "2010-07-16",
+     *     "trailerlink": "https://youtube.com/watch?v=YoHD9XEInc0",
+     *     "poster": "https://image.url/poster.jpg",
+     *     "genres": ["Action", "Sci-Fi"],
+     *     "backdrops": ["url1", "url2"],
+     *     "reviewIds": []
+     * }
+     */
+    @PostMapping
+    public ResponseEntity<Movies> createMovie(@RequestBody Movies movie) {
+        try {
+            Movies createdMovie = movieService.createMovie(movie);
+            return new ResponseEntity<>(createdMovie, HttpStatus.CREATED);
+        } catch (RuntimeException e) {
+            // En cas de doublon ou autre erreur
+            return new ResponseEntity<>(null, HttpStatus.CONFLICT);
+        }
+    }
+
+    /**
+     * DELETE - Supprimer un film par son ObjectId
+     * URL: /api/v1/movies/id/{id}
+     * Méthode: DELETE
+     */
+    @DeleteMapping("/id/{id}")
+    public ResponseEntity<String> deleteMovieById(@PathVariable ObjectId id) {
+        boolean deleted = movieService.deleteMovie(id);
+        if (deleted) {
+            return new ResponseEntity<>("Film supprimé avec succès", HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>("Film non trouvé", HttpStatus.NOT_FOUND);
+        }
+    }
+
+    /**
+     * DELETE - Supprimer un film par son imdbId
+     * URL: /api/v1/movies/{imdbId}
+     * Méthode: DELETE
+     */
+    @DeleteMapping("/{imdbId}")
+    public ResponseEntity<String> deleteMovieByImdbId(@PathVariable String imdbId) {
+        boolean deleted = movieService.deleteMovieByImdbId(imdbId);
+        if (deleted) {
+            return new ResponseEntity<>("Film supprimé avec succès", HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>("Film non trouvé", HttpStatus.NOT_FOUND);
+        }
+    }
+}
 }
