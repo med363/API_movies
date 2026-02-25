811eb7d29282985cdada550d37f2da57dc42c228
med363 <blibechmohamedamine97@gmail.com>
2026-02-25 11:30:32 +0100

    auto injection svc in controler to able to manage data & impliment method of svc in controller

--- src/main/java/dev/farhan/movies/MovieController.java
+++ src/main/java/dev/farhan/movies/MovieController.java
@@ -7,6 +7,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 /*fix root*/
 @RequestMapping("/api/v1/movies")
@@ -16,8 +18,8 @@ public class MovieController {
     private MovieService movieService;
     /*get all*/
     @GetMapping
-    public ResponseEntity<String> getAllMovies() {
-        return new ResponseEntity<String>("All movies", HttpStatus.OK);
+    public ResponseEntity<List<Movies>> getAllMovies() {
+        return new ResponseEntity<List<Movies>>(movieService.getAllMovies(), HttpStatus.OK);
     }
 
     /*pull data from databse so we need service so we will create repository*/
