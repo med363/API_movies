36dfd92743c226a70e97248c631cc69fd4a96489
med363 <blibechmohamedamine97@gmail.com>
2026-02-25 11:22:31 +0100

    we creat first src for get all  movies

--- src/main/java/dev/farhan/movies/MovieService.java
+++ src/main/java/dev/farhan/movies/MovieService.java
@@ -1,10 +1,24 @@
 package dev.farhan.movies;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-@Service
-public class MovieService {
-    public List<Movie> getAllMovies(){
+import java.util.List;
 
+@Service  // Cette annotation indique que cette classe est un Service Spring
+// Un Service contient la logique métier de l'application
+public class MovieService {
+
+    @Autowired  // Cette annotation permet l'injection automatique de dépendance
+    // Spring va créer et injecter automatiquement une instance de MovieRepository
+    private MovieRepository movieRepository;  // Repository pour accéder aux données des films
+
+    /**
+     * Méthode qui devrait retourner la liste de tous les films
+     * Actuellement, cette méthode est vide et ne retourne rien
+     * Elle devra être implémentée pour utiliser movieRepository et retourner List<Movies>
+     */
+    public List<Movies> getAllMovies(){
+        return movieRepository.findAll();
     }
 }
