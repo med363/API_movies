99662d6ea22fad35e6f3f731a798b60f7f0f77d4
med363 <blibechmohamedamine97@gmail.com>
2026-02-26 17:52:41 +0100

    add first request axios to get movies

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
-
+import api from './api/axiosConfig';
+import { useState, useEffect } from 'react';
 function App() {
+  const [movies, setMovies] = useState();
+
+  const getMovies = async () => {
+    const res= await api.get("/api/v1/movies");
+    setMovies(res.data);
+  }
+  useEffect(() => {
+    getMovies();
+  }, []);
   return (
     <div className="App">
 <h1>Welcome to my React App!</h1>
