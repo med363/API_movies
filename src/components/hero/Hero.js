53a828f2fe777f8dfa91324167a909ab3a31e50e
med363 <blibechmohamedamine97@gmail.com>
2026-02-28 14:53:53 +0100

    create child compp for home comp

---
+++ src/components/hero/Hero.js
@@ -0,0 +1,26 @@
+//child component of home, it will display the hero section of the home page, it will receive the movies data as a prop from the home component and display the hero section accordingly.
+import React from 'react'
+import './hero.css'
+import Carousel from 'react-material-ui-carousel'
+import { Paper, Button } from '@mui/material'   
+const Hero = ({ movies }) => {
+  return (
+    <div>
+        <Carousel>
+            {movies && movies.map((movie) => (
+                <Paper key={movie.id}>
+                    <img src={movie.posterUrl} alt={movie.title} className='hero-image' />
+                    <h2>{movie.title}</h2>
+                    <p>{movie.description}</p>
+                    <Button className="CheckButton">
+                        Check it out!
+                    </Button>
+                </Paper>
+            ))
+            }
+        </Carousel>
+    </div>
+  )
+}
+
+export default Hero
