9b0bd8c6aa88508b37513d76a200808bbb665977
med363 <blibechmohamedamine97@gmail.com>
2026-02-28 14:55:58 +0100

    pass props movie from home to hero child

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,8 +1,9 @@
 import React from 'react'
+import Hero from '../hero/Hero'
 // The Home component is a functional component that receives a prop called movies. It currently renders a simple div with the text "Home". You can modify this component to display the movies data as needed.
 const Home = ({ movies }) => {
   return (
-    <div>Home</div>
+    <Hero movies={movies} />
   )
 }
 
