97c19fecbc3380a428a914d95700879c49444625
med363 <blibechmohamedamine97@gmail.com>
2026-02-28 15:05:02 +0100

    affiche imge of movies

--- src/components/hero/Hero.js
+++ src/components/hero/Hero.js
@@ -9,12 +9,9 @@ const Hero = ({ movies }) => {
         <Carousel>
             {movies && movies.map((movie) => (
                 <Paper key={movie.id}>
-                    <img src={movie.posterUrl} alt={movie.title} className='hero-image' />
+                    <img src={movie.poster} alt={movie.title} className='hero-image' />
                     <h2>{movie.title}</h2>
                     <p>{movie.description}</p>
-                    <Button className="CheckButton">
-                        Check it out!
-                    </Button>
                 </Paper>
             ))
             }
