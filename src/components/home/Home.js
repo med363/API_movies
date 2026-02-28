3d4104dc10c117d58801d06cfe06fb38670680ba
med363 <blibechmohamedamine97@gmail.com>
2026-02-28 14:10:26 +0100

    add routes components for test

---
+++ src/components/home/Home.js
@@ -0,0 +1,9 @@
+import React from 'react'
+// The Home component is a functional component that receives a prop called movies. It currently renders a simple div with the text "Home". You can modify this component to display the movies data as needed.
+const Home = ({ movies }) => {
+  return (
+    <div>Home</div>
+  )
+}
+
+export default Home
